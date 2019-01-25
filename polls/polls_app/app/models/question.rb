31507955a0bf7961c37e780ff814fb0e2f069534
DrewE <andrew.engelstein@gmail.com>
2019-01-24 17:27:53 -0800

    Add through associations for Question and Response classes, Add #sibling_response to Reponse class

--- polls/polls_app/app/models/question.rb
+++ polls/polls_app/app/models/question.rb
@@ -21,5 +21,10 @@ belongs_to :poll,
 has_many :answer_choices,
   primary_key: :id,
   foreign_key: :question_id,
-  class_name: :AnswerChoice   
+  class_name: :AnswerChoice
+
+has_many :responses,
+  through: :answer_choices,
+  source: :responses
+  
 end
