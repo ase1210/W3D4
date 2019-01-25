c65637c5589804fc66b27eae8fe092f46583e0c9
DrewE <andrew.engelstein@gmail.com>
2019-01-24 17:28:32 -0800

    Committing final untracked changes

--- skeleton/movie_buff/02_queries.rb
+++ skeleton/movie_buff/02_queries.rb
@@ -1,4 +1,4 @@
-def eighties_b_movies
+ealdef eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between
   # 3 and 5 (inclusive).
   # Show the id, title, year, and score.
