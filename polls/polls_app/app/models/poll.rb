3ab329292f54802db3f9a89b35fb283570bb12d7
DrewE <andrew.engelstein@gmail.com>
2019-01-24 16:28:38 -0800

    Create associations for models: user, poll, question, answer_choice, response

--- polls/polls_app/app/models/poll.rb
+++ polls/polls_app/app/models/poll.rb
@@ -15,5 +15,9 @@ class Poll < ApplicationRecord
      foreign_key: :author_id,
      class_name: :User
 
+  has_many :questions,
+     primary_key: :id,
+     foreign_key: :poll_id,
+     class_name: :Question
 
 end
